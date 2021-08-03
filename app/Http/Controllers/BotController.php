21c6dd7ba4fec2bab89873ef6c7a51e4904fa611
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-03 20:28:30 +0800

    .

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -24,7 +24,7 @@ class BotController extends Controller
 
          // $response = Telegram::getWebhookUpdates();
 
-         $response = Telegram::getUpdate();
+         $response = Telegram::getUpdates();
 
          dd($response);
     }
