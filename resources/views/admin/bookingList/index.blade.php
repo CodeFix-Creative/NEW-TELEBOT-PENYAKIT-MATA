9d0fd2cefd338b6968fa3fee5dc412ea9821ae12
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-03 15:47:21 +0800

    .

--- resources/views/admin/bookingList/index.blade.php
+++ resources/views/admin/bookingList/index.blade.php
@@ -115,7 +115,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="tabahdatalabel">Upload Data Excel Spare Part</h5>
+                <h5 class="modal-title" id="tabahdatalabel">Filter Booking List Sesuai Tanggal</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
