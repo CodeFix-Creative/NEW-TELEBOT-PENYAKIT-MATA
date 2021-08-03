e61ad8f82c521aa4a8f1449b8b9dd5f620162d7a
FarrelArrahman <arrahman_farrel@yahoo.com>
2021-08-03 17:39:04 +0800

    Add Menu For Telegram Bot

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -30,7 +30,7 @@ class TelegramController extends Controller
             $this->apiRequest('sendMessage', [
                 'chat_id' => $userId,
                 'text' => $text,
-                'reply_markup' => $this->keyboardBtn($mainMenu),
+                'reply_markup' => $this->keyboardBtn($this->mainMenu),
             ]);
 
         } else if ($action == 'Cek Service') {
@@ -63,7 +63,7 @@ class TelegramController extends Controller
             $this->apiRequest('sendMessage', [
                 'chat_id' => $userId,
                 'text' => $text,
-                'reply_markup' => $this->keyboardBtn($mainMenu),
+                'reply_markup' => $this->keyboardBtn($this->mainMenu),
             ]);
         }
     }
