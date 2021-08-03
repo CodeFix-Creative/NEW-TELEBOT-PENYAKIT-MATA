d5a522dd043097bca01aecaa111e34ad438e7c90
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-03 21:40:45 +0800

    test

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,6 +15,7 @@ class VerifyCsrfToken extends Middleware
         '/1907491983:AAGY5f9Cm-JNc2g1TswjHrpWSmU2_YZJWYA/webhook',
         '/admin/test-bot',
         '/*',
+        '/webhook',
       //   'https://asus-service-center.online/admin/test-bot',
     ];
 }
