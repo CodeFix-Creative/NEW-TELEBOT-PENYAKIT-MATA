b25d6eb724085f09a5bb7bca10576c3772e69d09
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-03 17:22:40 +0800

    test

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -31,6 +31,14 @@ class TelegramController extends Controller
                 'reply_markup' => $this->keyboardBtn($option),
             ]);
         }
+
+        if($action == '/test') {
+           $this->apiRequest('sendMessage', [
+                'chat_id' => $userId,
+                'text' => 'test terrrooooosss!!',
+               //  'reply_markup' => $this->keyboardBtn($option),
+            ]);
+        }
     }
 
     public function webhook()
