52d698b685879e5b1dbd06960d1a90802cd5ff01
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-03 21:25:45 +0800

    .

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -30,6 +30,11 @@ class BotController extends Controller
 
          // $response = Telegram::getUpdates();
 
+         $response = $telegram->setWebhook([
+            'url' => 'https://asus-service-center.online/1907491983:AAGY5f9Cm-JNc2g1TswjHrpWSmU2_YZJWYA/webhook',
+            'certificate' => '/path/to/public_key_certificate.pub'
+         ]);
+
          dd($response);
     }
 
