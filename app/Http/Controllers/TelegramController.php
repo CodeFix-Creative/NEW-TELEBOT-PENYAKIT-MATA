111c090956fe97c5e461c1219ffdaa8e862afc2b
FarrelArrahman <arrahman_farrel@yahoo.com>
2021-08-03 18:04:09 +0800

    Add Menu For Telegram Bot

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -49,15 +49,13 @@ class TelegramController extends Controller
 
             foreach($part as $key => $value) {
                 $text .= "\n " . $key + 1 . ". " . $value->product_group;
-                $numberOption[] = [$key + 1];
             }
 
             $this->apiRequest('sendMessage', [
                 'chat_id' => $userId,
                 'text' => $text,
-                'reply_markup' => $this->keyboardBtn($numberOption),
             ]);
-            
+
         } else if ($action == "Booking Service") {
             $text = "Anda memilih menu booking service.";
 
