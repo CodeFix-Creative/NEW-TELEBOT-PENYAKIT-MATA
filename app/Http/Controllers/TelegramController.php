c87f3a03ebeaf01b3bca196364101a64944d04aa
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-04 13:48:23 +0800

    .

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -21,8 +21,10 @@ class TelegramController extends Controller
     public function index()
     {
         $result = json_decode(file_get_contents('php://input'));
-        $action = $result->message->text;
-        $userId = $result->message->from->id;
+      //   $action = $result->message->text;
+        $action = "";
+      //   $userId = $result->message->from->id;
+        $userId = "";
 
         if($action == "/start") {
             $text = "Selamat datang di Bot Telegram ASUS Service Center. Silahkan pilih menu di bawah ini: ";
