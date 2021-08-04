9d0e1b59d3ab752da827db679b033323518369af
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-04 21:57:20 +0800

    .

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -57,13 +57,58 @@ class TelegramController extends Controller
                 'text' => $text,
             ]);
         } else if (Service::where('rma_no_1' , $action)->exists() == true) {
-            $text = "Mantap Benar! \n";
+            
+            $status = Service::where('rma_no_1' , $action)->first();
+
+            if ($status->status_1 == "TRANSFER") {
+               $text = "**Informasi Umum/Unit** \n";
+               $text .= "1. RMA NO 1        : ". $status->rma_no_1 ."\n";
+               $text .= "2. SERIAL NO       : ". $status->serial_no ."\n";
+               $text .= "3. RMA ISSUE DATE  : ". $status->rma_issue_date ."\n";
+               $text .= "4. PRODUCT TYPE DESC : ". $status->product_type_desc ."\n";
+               $text .= "5. MODEL ID        : ". $status->model_id ."\n";
+               $text .= "6. WARRANTY_END    : ". $status->warranty_end ."\n";
+               $text .= "7. WARRANTY_STATUS : ". $status->warranty_status ."\n";
+               $text .= "8. REMARK/PROBLEM  : ". $status->remark_or_problem ."\n\n";
+
+               $text = "**Informasi Status Service** \n";
+               $text .= "1. STATUS 1        : ". $status->status_1 ."\n";
+               $text .= "2. TRANSFER SHIP SUBMIT DATE : ". $status->transfer_ship_submit_date ."\n";
+               $text .= "3. RMA CENTER 2    : ". $status->rma_center_2 ."\n";
+               $text .= "4. RMA NO 2        : ". $status->rma_no_2 ."\n";
+               $text .= "5. STATUS 2        : ". $status->status_2 ."\n";
+               $text .= "6. FINAL RMA STATUS: ". $status->final_rma_status ."\n";
+            }else{
+               $text = "**Informasi Umum/Unit** \n";
+               $text .= "1. RMA NO 1        : ". $status->rma_no_1 ."\n";
+               $text .= "2. SERIAL NO       : ". $status->serial_no ."\n";
+               $text .= "3. RMA ISSUE DATE  : ". $status->rma_issue_date ."\n";
+               $text .= "4. PRODUCT TYPE DESC : ". $status->product_type_desc ."\n";
+               $text .= "5. MODEL ID        : ". $status->model_id ."\n";
+               $text .= "6. WARRANTY_END    : ". $status->warranty_end ."\n";
+               $text .= "7. WARRANTY_STATUS : ". $status->warranty_status ."\n";
+               $text .= "8. REMARK/PROBLEM  : ". $status->remark_or_problem ."\n\n";
+
+               $text = "**Informasi Status Service** \n";
+               $text .= "1. STATUS 1        : ". $status->status_1 ."\n";
+               $text .= "2. KBO STATUS      : ". $status->kbo_status ."\n";
+               $text .= "3. ORDER DATE      : ". $status->order_date ."\n";
+               $text .= "4. ORG PART DESC   : ". $status->org_part_desc ."\n";
+               $text .= "5. NEW PART NO     : ". $status->new_part_no ."\n";
+               $text .= "6. NEW PART DESC   : ". $status->new_part_desc ."\n";
+               $text .= "7. ALLOCATED DATE  : ". $status->allocated_date ."\n";
+               $text .= "8. KBO ETA END     : ". $status->kbo_eta_end ."\n";
+               $text .= "9. FINAL RMA STATUS: ". $status->final_rma_status ."\n";
+            }
+
+            // $text = "Mantap Benar! \n";
             // $text .= "Silahkan inputkan nomor RMA anda :";
 
             $this->apiRequest('sendMessage', [
                 'chat_id' => $userId,
                 'text' => $text,
             ]);
+            
         } else if ($action == "Cek Spare Part") {
             $part = Part::select('product_group')->distinct()->get();
             $text = "Silahkan pilih product group: \n";
