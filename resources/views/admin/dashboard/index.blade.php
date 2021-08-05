fb4c182d526e488b7d3be3ecf4c6f62ca38e6629
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-06 00:41:37 +0800

    .

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -14,14 +14,14 @@
 @section('content')
 <div class="row mt--2">
     <!-- Statistic -->
-    <div class="col-md-3">
+    <div class="col-md-4">
         <div class="card full-height">
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-3">
                         <i class="fas fa-user-cog fa-5x"></i>
                     </div>
-                    <div class="col-md-9">
+                    <div class="col-md-8">
                         <h2>Dashboard</h2>
                         <h5>Keterangan</h5>
                     </div>
@@ -29,14 +29,14 @@
             </div>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-4">
         <div class="card full-height">
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-3">
                         <i class="fas fa-user-cog fa-5x"></i>
                     </div>
-                    <div class="col-md-9">
+                    <div class="col-md-8">
                         <h2>Dashboard</h2>
                         <h5>Keterangan</h5>
                     </div>
@@ -44,14 +44,14 @@
             </div>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-4">
         <div class="card full-height">
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-3">
                         <i class="fas fa-user-cog fa-5x"></i>
                     </div>
-                    <div class="col-md-9">
+                    <div class="col-md-8">
                         <h2>Dashboard</h2>
                         <h5>Keterangan</h5>
                     </div>
@@ -59,14 +59,29 @@
             </div>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-4">
         <div class="card full-height">
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-3">
                         <i class="fas fa-user-cog fa-5x"></i>
                     </div>
-                    <div class="col-md-9">
+                    <div class="col-md-8">
+                        <h2>Dashboard</h2>
+                        <h5>Keterangan</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card full-height">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-3">
+                        <i class="fas fa-user-cog fa-5x"></i>
+                    </div>
+                    <div class="col-md-8">
                         <h2>Dashboard</h2>
                         <h5>Keterangan</h5>
                     </div>
