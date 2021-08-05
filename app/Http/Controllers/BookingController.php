71d9f6b7a8173447af2feab900cb2ef25518c827
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-05 20:52:51 +0800

    .

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Booking;
 use App\Models\CustomerService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;   
 use Carbon\Carbon;
 
 class BookingController extends Controller
