7df1f9b3a578a3878f34f6db464052cc6823abb6
achmadtaufikhidayatullah <taufik.sunmedia@gmail.com>
2021-08-11 19:43:56 +0800

    add caption booking service

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -297,7 +297,8 @@ class TelegramController extends Controller
             } else {
                 $text = "Anda memilih menu booking service. \n";
                 $text .= "Silahkan pilih waktu yang tersedia. \n";
-                $text .= "Jika tidak muncul, berarti booking service sudah full. Silahkan datang langsung ke Asus Service Center terdekat. \n";
+                $text .= "Jika tidak muncul, berarti booking service sudah full. Silahkan datang langsung ke Asus Service Center terdekat. \n\n";
+                $text .= "Jadwal akan didapatkan 1 Hari kerja setelah booking service dilakukan (Hari kerja Senin-Jumat). \n";
 
                 $customerService = CustomerService::all();
                 $bookingTimeAvailableArr = [];
