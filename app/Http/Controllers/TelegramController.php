28acbd7cf4e6a75e708764de656fc744c843bc0c
achmadtaufikhidayatullah <achmadtaufikhidayatullah6@gmail.com>
2023-10-18 14:32:51 +0800

    Gejala

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -30,8 +30,8 @@ class TelegramController extends Controller
         $action = $result->message->text;
         $userId = $result->message->from->id;
 
-        // Part
         
+        // Inisiasi Reply
         if($action == "/start") {
             $text = "Selamat datang di Bot Telegram DETEKSI PENYAKIT MATA . Silahkan pilih menu di bawah ini: ";
 
@@ -40,7 +40,8 @@ class TelegramController extends Controller
                 'text' => $text,
                 'reply_markup' => $this->keyboardBtn($this->mainMenu),
             ]);
-
+        
+        // Response History Diagnosa Saya
         }else if ($action == "History Diagnosa Saya"){
             $text = "Anda memilih menu History Diagnosa Saya \n";
             $text .= "Silahkan beritahu kami nomor telephone yang pernah anda masukan :";
@@ -49,6 +50,45 @@ class TelegramController extends Controller
                 'chat_id' => $userId,
                 'text' => $text,
             ]);
+
+        // Response Diagnosa Penyakit
+        }else if ($action == "Diagnosa Penyakit"){
+            $text = "Anda memilih menu Diagnosa Penyakit \n";
+            $text .= "Silahkan beritahu kami data diri anda terlebih dahulu \n";
+            $text .= "Nama Lengkap#Umur Anda#Jenis Kelamin#NomorTelephone\n\n";
+            $text .= "Contoh\n";
+            $text .= "Tifani Angga#23#L#081231XXXXXXX\n\n";
+
+            $this->apiRequest('sendMessage', [
+                'chat_id' => $userId,
+                'text' => $text,
+            ]);
+
+        // Response Setelah Memasukan Biodata
+        }else if(strpos($action, '#') == true){
+          $customerData = explode("#", $action);
+
+          $diagnosa = new Diagnosa;
+          $diagnosa->nama = $customerData[0];
+          $diagnosa->umur = $customerData[1];
+          $diagnosa->jenis_kelamin = $customerData[2];
+          $diagnosa->nomor_telephone = $customerData[3];
+          $diagnosa->save();
+
+          $text = "Data diri anda berhasil disimpan \n";
+          $text .= "Silahkan pilih gajala penyakit yang anda rasakan : \n";
+
+          $gejala = Gejala::where('status' , 'Aktif')->get();
+
+          $gejalaKeyboard[] = [$gejala->nama_gejala];
+
+          $this->apiRequest('sendMessage', [
+              'chat_id' => $userId,
+              'text' => $text,
+              'reply_markup' => $this->keyboardBtn($this->mainMenu),
+          ]);
+
+        // Check History Diagnosa Kalau Ada
         }else if (Diagnosa::where('nomor_telephone' , $action)->exists() == true){
             $text = "Berikut Adalah History terakhir diagnosa penyakit anda \n";
 
@@ -57,6 +97,8 @@ class TelegramController extends Controller
                 'text' => $text,
                 'reply_markup' => $this->keyboardBtn($this->mainMenu),
             ]);
+
+        // Check History Diagnosa Kalau Tidak Ada
         }else if (Diagnosa::where('nomor_telephone' , $action)->exists() == false){
             $text = "Anda belum memiliki history diagnosa penyakit \n";
             $text .= "Silahkan lakukan konsultasi diagnosa penyakit anda terlebih dahulu";
@@ -66,6 +108,8 @@ class TelegramController extends Controller
                 'text' => $text,
                 'reply_markup' => $this->keyboardBtn($this->mainMenu),
             ]);
+
+        
         } else {
             $text = "maaf, input yang anda masukkan salah, silahkan input sesuai format atau silahkan pilih menu dibawah ini: ";
 
